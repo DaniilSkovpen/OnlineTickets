96c514d87fd584529934471173eb412ee5ff98c6
Daniil Skovpen <skovpen000@gmail.com>
2022-11-28 18:33:14 +0200

    Create Produser.cs

---
+++ OnlineTickets/Models/Produser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineTickets.Models
+{
+    public class Produser
+    {
+        [Key]
+        public int ActorsId { get; set; }
+
+        public string ProfilePicturesURL { get; set; }
+        public string FullName { get; set; }
+        public string Bios { get; set; }
+    }
+}
