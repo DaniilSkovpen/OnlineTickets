ace47a5705e17a31be41566f67c078e17d5f5fcb
Daniil Skovpen <skovpen000@gmail.com>
2022-11-28 18:35:11 +0200

    Create Cinema.cs

---
+++ OnlineTickets/Models/Cinema.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineTickets.Models
+{
+    public class Cinema
+    {
+        [Key]
+        public int CinemasId { get; set; }
+
+        public string Logo { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
