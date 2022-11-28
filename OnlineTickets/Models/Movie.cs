3ffa6ed669915c9fb808e14826702c17fe8b229a
Daniil Skovpen <skovpen000@gmail.com>
2022-11-28 18:44:51 +0200

    Createt Movie.cs module and a new folder with MovieCategory.cs

---
+++ OnlineTickets/Models/Movie.cs
@@ -0,0 +1,22 @@
+﻿using OnlineTickets.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineTickets.Models
+{
+    public class Movie
+    {
+        [Key]
+        public int MovieId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public string ImgURL { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public MovieCategory MovieCategory { get; set; }
+    }
+}
