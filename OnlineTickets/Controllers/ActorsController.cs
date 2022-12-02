49d1989c849ff646e69b4f6166dd76c9fb2cec76
Daniil Skovpen <skovpen000@gmail.com>
2022-12-02 19:58:24 +0200

    Adding an ActorsController.cs and creating folder Views\Actors

---
+++ OnlineTickets/Controllers/ActorsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineTickets.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineTickets.Controllers
+{
+    public class ActorsController : Controller
+    {
+
+        private readonly AppDBContext _context;
+
+        public ActorsController(AppDBContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var data = _context.Actors.ToList();
+            return View();
+        }
+    }
+}
