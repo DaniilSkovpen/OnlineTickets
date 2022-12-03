b7a4347a36ae2ed15e90468d63e9fe3a12870f2f
Daniil Skovpen <skovpen000@gmail.com>
2022-12-04 00:48:26 +0200

    Adding Cinemas/Index.cshtml, updated Cinema.cs

--- OnlineTickets/Models/Cinema.cs
+++ OnlineTickets/Models/Cinema.cs
@@ -11,8 +11,11 @@ namespace OnlineTickets.Models
         [Key]
         public int CinemasId { get; set; }
 
+        [Display(Name = "Cinema Logo")]
         public string Logo { get; set; }
+        [Display(Name = "Cinema Name")]
         public string Name { get; set; }
+        [Display(Name = "Cinema Description")]
         public string Description { get; set; }
 
         //Relationships
