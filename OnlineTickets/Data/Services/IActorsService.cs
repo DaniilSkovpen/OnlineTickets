1535407c892637c5bb8bd3e51eae5408c77ec398
Daniil Skovpen <skovpen000@gmail.com>
2022-12-05 14:36:19 +0200

    Adding IActorsService interface

---
+++ OnlineTickets/Data/Services/IActorsService.cs
@@ -0,0 +1,17 @@
+﻿using OnlineTickets.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineTickets.Data.Services
+{
+    public interface IActorsService
+    {
+        IEnumerable<Actor> GetAll();
+        Actor GetBy(int id);
+        void Add(Actor actor);
+        void Update(int id, Actor newActor);
+        void Delete(int id);
+    }
+}
