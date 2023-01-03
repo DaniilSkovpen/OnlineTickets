a28c67974806c68d04d23a97262b467471e14a04
Daniil Skovpen <skovpen000@gmail.com>
2023-01-03 18:25:48 +0200

    AddAsync() method in base repository

--- OnlineTickets/Data/Base/EntityBaseRepository.cs
+++ OnlineTickets/Data/Base/EntityBaseRepository.cs
@@ -9,28 +9,17 @@ namespace OnlineTickets.Data.Base
     public class EntityBaseRepository<T> : IEntityBaseRepository<T> where T : class, IEntityBase, new()
     {
         private readonly AppDBContext _context; 
-        public Task AddAsync(T entity)
-        {
-            throw new NotImplementedException();
-        }
-
+        public async Task AddAsync(T entity) => await _context.Set<T>().AddAsync(entity);
+        
         public Task DeleteAsync(int id)
         {
             throw new NotImplementedException();
         }
 
-        public async Task<IEnumerable<T>> GetAllAsync()
-        {
-            var resoult = await _context.Set<T>().ToListAsync();
-            return resoult;
-        }
-
-        public async Task<T> GetByIdAsync(int id)
-        {
-            var resoult = await _context.Set<T>().FirstOrDefaultAsync(n => n.Id == id);
-            return resoult;
-        }
-
+        public async Task<IEnumerable<T>> GetAllAsync() => await _context.Set<T>().ToListAsync();
+       
+        public async Task<T> GetByIdAsync(int id) => await _context.Set<T>().FirstOrDefaultAsync(n => n.Id == id);
+       
         public Task<T> UpdateAsync(int id, T entity)
         {
             throw new NotImplementedException();
